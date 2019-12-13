ddf6f223f020179b0f9a6fdfbe6e0638c4d742a1
MatD3mons <matthieu.desmarescaux@isen.yncrea.fr>
2019-12-13 12:40:43 +0100

    MatD3mons
    Déplacement dans un Thread

---
+++ src/Exercice.py
@@ -0,0 +1,141 @@
+import numpy as np
+from Video import video
+from Camera import camera
+from threading import Thread
+import utile
+import screeninfo
+import posenet
+import argparse
+import cv2
+import math
+from math import sqrt
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+MIN = 0.01
+
+MOT_DOUX = ["Tres Bien", "Bien", "Assez bien", "Courage"]
+
+IMAGE = [cv2.imread('img/verygood.png'),
+        cv2.imread('img/good.png'),
+        cv2.imread('img/bad.png'),
+        cv2.imread('img/courage.png')]
+
+class exercice(Thread):
+
+    """Thread chargé simplement d'afficher une lettre dans la console."""
+
+    def __init__(self,sess,model_cfg,model_outputs,args,cheminV,cheminN):
+        self.arret = True
+        self.Camera_thread = camera(args, sess, model_cfg, model_outputs)
+        self.Video_thread = video(MOT_DOUX, cheminV, cheminN)
+        self.VideoPoint = None
+        self.CameraPoint = None
+        self.Camera = None
+        self.data = None
+        self.Video = None
+        self.check = False
+        self.i = len(IMAGE)-1
+        self.graph = []
+        self.fig = plt.figure(figsize=(8, 2), dpi=80)
+        self.listSum = []
+        self.sum = 0
+        Thread.__init__(self)
+
+    def run(self):
+        """Code à exécuter pendant l'exécution du thread."""
+        self.Camera_thread.start()
+        self.Video_thread.start()
+        while self.arret:
+            if len(self.Camera_thread.ListPoint) != 0:#On check si on a une donnée camera
+                self.Camera = self.Camera_thread.ListPoint[0][0]
+                self.CameraPoint = utile.Patron(self.Camera_thread.ListPoint[0][1][0], False)
+                del self.Camera_thread.ListPoint[0]
+                if self.Camera > MIN:
+                    self.Camera_thread.frame_count += 1
+                self.check = True
+
+            if len(self.Video_thread.ListPoint) != 0 and self.Camera is not None:# " " " video
+                if self.Camera > MIN:
+                    self.Video = self.Video_thread.ListPoint[0][0]
+                    self.VideoPoint = self.Video_thread.ListPoint[0][1]
+                    del self.Video_thread.ListPoint[0]
+                    self.VideoPoint = utile.Patron(self.VideoPoint[:, 1], True)
+                    self.Video_thread.frame_count += 1
+
+            if self.Video is not None:#On check si il y en a deux
+                if self.check:
+                    print("Camera : " + str(self.Camera_thread.frame_count)+" -- Video : " + str(self.Video_thread.frame_count))
+                    if self.Camera > MIN:
+                        Comparatif = [5,6,7,8,9,10,11,12,13,14,15,16]
+                        for p in Comparatif:
+                            self.sum += (utile.Distance(self.CameraPoint[p],self.VideoPoint[p])/(sqrt(2)*len(Comparatif)))#Normalisation
+                    else:
+                        self.sum += 1
+                    if (self.Camera_thread.frame_count)%10 == 0:
+                        self.sum = self.sum / 10
+                        self.listSum.append(self.sum)
+                        if self.sum < 0.225:
+                            self.i = 0
+                        elif self.sum < 0.275:
+                            self.i = 1
+                        elif self.sum < 0.30:
+                            self.i = 2
+                        else:
+                            self.i = 3
+                        self.sum = 0
+                    self.check = False
+                else:
+                    print("Camera : " + str(self.Camera_thread.frame_count) +
+                          " == Video : " + str(self.Video_thread.frame_count))
+    #==============================================================================#
+                if (self.Camera_thread.frame_count)%10 == 0:
+                    plt.cla()
+                    plt.ylim(0,1)
+                    canvas = FigureCanvas(self.fig)
+                    plt.plot(range(0, len(self.listSum)), self.listSum)
+                    canvas.draw()
+                    self.data = np.frombuffer(self.fig.canvas.tostring_rgb(), dtype=np.uint8)
+                    self.data = self.data.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
+                if self.data is not None:
+                    self.Video[0:self.data.shape[0], self.Video.shape[1] - self.data.shape[1]:self.Video.shape[1]] = self.data
+                    cv2.putText(self.Video, str(MOT_DOUX[self.i]), (50, 200),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 4)
+                    cv2.putText(self.Video, str("Marge d'erreurs:")+str(round(self.listSum[len(self.listSum)-1],2)), (50, 100),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 4)
+    #==============================================================================#
+                    img = IMAGE[self.i]
+                    self.Video[250:img.shape[0]+250, img.shape[1]+100 -img.shape[1]:img.shape[1]+100] = img
+    #==============================================================================#
+                self.Video[self.Video.shape[0]-500:self.Video.shape[0], 0:500] = [0, 0, 0]
+    #==============================================================================#
+                self.Video = utile.draw(self.VideoPoint,self.Video,[0, 255, 255],True)
+                if self.Camera > 0:
+                    self.Video = utile.draw(self.CameraPoint,self.Video,[255, 255, 0],False)
+                else:#Si la personne sort du cadre de la caméra
+                    self.Video[:,:] = [100,100,100]
+                    cv2.putText(self.Video, str("REVENEZ DEVANT LA CAMERA"), (50, 500),cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 0), 12)
+    #==============================================================================#
+                cv2.namedWindow('Video', cv2.WND_PROP_FULLSCREEN)
+                cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                cv2.imshow('Video', self.Video)
+    #==============================================================================#
+            if self.Video_thread.Taille is not None:
+                if self.Video_thread.Taille == self.Video_thread.frame_count:
+                    self.Video_thread.stopthread()
+                    self.Camera_thread.stopthread()
+                    break
+    #==============================================================================#
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                self.stopthread()
+                break
+
+        b = 0
+        for i in self.listSum:
+            b = b + i
+        print(" !=! "+str(b / (int(self.Video_thread.frame_count/10))*100)+str(" %")+" !=! ")
+
+    def stopthread(self):
+        self.Video_thread.stopthread()
+        self.Camera_thread.stopthread()
+        self.arret = False
--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,10 @@
 import numpy as np
-from Video import video
-from Camera import camera
-import utile
-import random
-import screeninfo
+from Exercice import exercice
 import posenet
 import argparse
 import cv2
-import math
-from math import sqrt
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-
-#==============================================================================#
-MIN = 0.01
 #==============================================================================#
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
@@ -26,126 +15,12 @@ parser.add_argument('--scale_factor', type=float, default=1)  # laissé à 1
 parser.add_argument('--file', type=str, default=None, help="Optionally use a video file instead of a live camera")
 args = parser.parse_args()
 #==============================================================================#
-MOT_DOUX = ["Tres Bien", "Bien", "Assez bien", "Courage"]
-#==============================================================================#
-IMAGE = [cv2.imread('img/verygood.png'),
-         cv2.imread('img/good.png'),
-         cv2.imread('img/bad.png'),
-         cv2.imread('img/courage.png')]
-#==============================================================================#
-#=============================== MAIN DU PROGRAMME ============================#
-#==============================================================================#
+
 def main():
     sess = tf.Session()
     model_cfg, model_outputs = posenet.load_model(args.model, sess)
-    Camera_thread = camera(args, sess, model_cfg, model_outputs)
-    Video_thread = video(MOT_DOUX, './Exercice1/Exercice1.mp4', './Exercice1/video.npy')
-#==============================================================================#
-    Camera = None
-    data = None
-    Video = None
-    VideoPoint = None
-    CameraPoint = None
-    rand = 0
-    check = False
-    i = 3
-    graph = []
-    fig = plt.figure(figsize=(8, 2), dpi=80)
-    listSum = []
-    sum = 0
-#==============================================================================#
-    Camera_thread.start()
-    Video_thread.start()
-#==============================================================================#
-    while True:
-        if len(Camera_thread.ListPoint) != 0:#On check si on a une donnée camera
-            Camera = Camera_thread.ListPoint[0][0]
-            CameraPoint = utile.Patron(Camera_thread.ListPoint[0][1][0], False)
-            del Camera_thread.ListPoint[0]
-            if Camera > MIN:
-                Camera_thread.frame_count += 1
-            check = True
-
-        if len(Video_thread.ListPoint) != 0 and Camera is not None:# " " " video
-            if Camera > MIN:
-                Video = Video_thread.ListPoint[0][0]
-                VideoPoint = Video_thread.ListPoint[0][1]
-                del Video_thread.ListPoint[0]
-                VideoPoint = utile.Patron(VideoPoint[:, 1], True)
-                Video_thread.frame_count += 1
-
-        if Video is not None:#On check si il y en a deux
-            if check:
-                print("Camera : " + str(Camera_thread.frame_count)+" -- Video : " + str(Video_thread.frame_count))
-                if Camera > MIN:
-                    Comparatif = [5,6,7,8,9,10,11,12,13,14,15,16]
-                    for p in Comparatif:
-                        sum = sum + (utile.Distance(CameraPoint[p],VideoPoint[p])/(sqrt(2)*len(Comparatif)))#Normalisation
-                else:
-                    sum = sum + 1
-                if (Camera_thread.frame_count)%10 == 0:
-                    sum = sum / 10
-                    listSum.append(sum)
-                    if sum < 0.225:
-                        i = 0
-                    elif sum < 0.275:
-                        i = 1
-                    elif sum < 0.30:
-                        i = 2
-                    else:
-                        i = 3
-                    sum = 0
-                check = False
-            else:
-                print("Camera : " + str(Camera_thread.frame_count) +
-                      " == Video : " + str(Video_thread.frame_count))
-#==============================================================================#
-            if (Camera_thread.frame_count)%10 == 0:
-                plt.cla()
-                plt.ylim(0,1)
-                canvas = FigureCanvas(fig)
-                plt.plot(range(0, len(listSum)), listSum)
-                canvas.draw()
-                data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-                data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-            if data is not None:
-                Video[0:data.shape[0], Video.shape[1] - data.shape[1]:Video.shape[1]] = data
-                cv2.putText(Video, str(MOT_DOUX[i]), (50, 200),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 4)
-                cv2.putText(Video, str("Marge d'erreurs:")+str(round(listSum[len(listSum)-1],2)), (50, 100),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 4)
-#==============================================================================#
-                img = IMAGE[i]
-                Video[250:img.shape[0]+250, img.shape[1]+100 -img.shape[1]:img.shape[1]+100] = img
-#==============================================================================#
-            Video[Video.shape[0]-500:Video.shape[0], 0:500] = [0, 0, 0]
-#==============================================================================#
-            Video = utile.draw(VideoPoint,Video,[0, 255, 255],True)
-            if Camera > 0:
-                Video = utile.draw(CameraPoint,Video,[255, 255, 0],False)
-            else:#Si la personne sort du cadre de la caméra
-                Video[:,:] = [100,100,100]
-                cv2.putText(Video, str("REVENEZ DEVANT LA CAMERA"), (50, 500),cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 0), 12)
-#==============================================================================#
-            cv2.namedWindow('Video', cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.imshow('Video', Video)
-#==============================================================================#
-        if Video_thread.Taille is not None:
-            if Video_thread.Taille == Video_thread.frame_count:
-                Video_thread.stopthread()
-                Camera_thread.stopthread()
-                break
-#==============================================================================#
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            Video_thread.stopthread()
-            Camera_thread.stopthread()
-            break
-#==============================================================================#
-    print("============= C'EST FINIIIIIIIII ===============")
-    b = 0
-    for i in listSum:
-        b = b + i
-    print(" !=! "+str(b / (int(Video_thread.frame_count/10))*100)+str(" %")+" !=! ")
-
+    Exercice1 = exercice(sess,model_cfg,model_outputs,args,'./Exercice1/Exercice1.mp4','./Exercice1/video.npy')
+    Exercice1.start()
 
 if __name__ == "__main__":
     main()
